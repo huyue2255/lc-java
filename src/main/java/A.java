d1b40070f37ca9116fa605cda686f5d997d18703
yuehu <huyue2255@gmail.com>
2017-09-15 22:48:41 -0700

    a

---
+++ src/main/java/A.java
@@ -0,0 +1,23 @@
+import com.google.gson.Gson;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by yuehu on 9/15/17.
+ */
+public class A {
+    public static List<String> f1() {
+        List<String> a = Arrays.asList("a", "b", "C");
+        return a;
+    }
+
+    @Test
+    public void t1() {
+
+        String s = new Gson().toJson(A.f1());
+        System.out.println(s);
+
+    }
+}
