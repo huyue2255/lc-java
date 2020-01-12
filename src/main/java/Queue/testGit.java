0fa0e935aa415cb242ef0a826bb3fc4b18d776a2
yuehu <huyue2255@gmail.com>
2020-01-11 17:10:11 -0800

    test git 2

---
+++ src/main/java/Queue/testGit.java
@@ -0,0 +1,11 @@
+package Queue;
+
+/**
+ * Created by yuehu on 1/11/20.
+ */
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("test git");
+        System.out.println("test git master");
+    }
+}
