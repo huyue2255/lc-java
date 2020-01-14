a7ada3301b91ec13c77a0631983e30c44d7292cc
yuehu <huyue2255@gmail.com>
2020-01-13 18:42:49 -0800

    master commit 2

--- src/main/java/Queue/testGit3.java
+++ src/main/java/Queue/testGit3.java
@@ -3,8 +3,8 @@ package Queue;
 /**
  * Created by yuehu on 1/13/20.
  */
-public class testGit2 {
+public class testGit3 {
     public static void main(String[] args) {
-        System.out.println("master commit 2");
+        System.out.println("master commit 3");
     }
 }
