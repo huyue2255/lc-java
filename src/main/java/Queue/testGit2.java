3916d42f77a00a9cc30fceb96f6ed3869744bb36
yuehu <huyue2255@gmail.com>
2020-01-13 18:41:49 -0800

    master commit 1

---
+++ src/main/java/Queue/testGit2.java
@@ -0,0 +1,10 @@
+package Queue;
+
+/**
+ * Created by yuehu on 1/13/20.
+ */
+public class testGit2 {
+    public static void main(String[] args) {
+        System.out.println("master commit 1");
+    }
+}
