4bca319496a2de57864fd08d2ca5e3753c774a07
yuehu <huyue2255@gmail.com>
2020-02-04 19:30:50 -0800

    laioffer 06

---
+++ src/main/java/LaiOffer/Bipartite.java
@@ -0,0 +1,15 @@
+package LaiOffer;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+/**
+ * Created by yuehu on 1/26/20.
+ */
+//public class Bipartite {
+//    public boolean isBipartite(List<GraphNode> graph) {
+//        HashMap<GraphNode, Integer> visited = new HashMap<>();
+//
+//    }
+//}
